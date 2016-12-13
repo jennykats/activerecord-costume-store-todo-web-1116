f0c2eac2692f20cc2a366468eccceaecdeafa634
Jenny <jenny.kats@aol.com>
2016-12-13 09:23:37 -0500

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,17 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume < ActiveRecord::Base
+  # def self.create_table
+  #   sql = <<-SQL
+  #   CREATE TABLE IF NOT EXISTS costumes (
+  #     id INTEGER PRIMARY KEY,
+  #     title TEXT,
+  #     length INTEGER
+  #   )
+  #   SQL
+  #
+  #   ActiveRecord::Base.connection.execute(sql)
+  # end
+
+end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,15 @@
 # Create your HauntedHouse class here
+
+class HauntedHouse < ActiveRecord::Base
+  # def self.create_table
+  #     sql = <<-SQL
+  #     CREATE TABLE IF NOT EXISTS haunted_houses (
+  #       id INTEGER PRIMARY KEY,
+  #       title TEXT,
+  #       length INTEGER
+  #     )
+  #     SQL
+  #
+  #     ActiveRecord::Base.connection.execute(sql)
+  #   end
+end
